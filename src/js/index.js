05bb7d39d0d9c6d1724043523d2d033b9de2ac41
тигран могров <tigranmogrov@gmail.com>
2020-02-03 15:38:59 +0200

    my project 154

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import './core/polyfill';
+import {boot} from './core/boot';
+import './main';
+boot.init([
+	// module
+]);
---
+++ src/js/main.js
@@ -0,0 +1,48 @@
+import {disableBodyScroll, enableBodyScroll} from 'body-scroll-lock/lib/bodyScrollLock.es6';
+import {delay} from 'rxjs/operators';
+
+function mobileMenu() {
+	let hamburger = document.getElementById('hamburger-btn');
+
+	let headerNav = document.getElementById('header-nav');
+
+	let state = false;
+
+	hamburger.addEventListener('click', function () {
+		if (state === false) {
+			openMenu();
+		} else {
+			closeMenu();
+		}
+	});
+
+
+	function openMenu() {
+		disableBodyScroll(headerNav);
+		headerNav.style.transition = '0.4s ease 0s';
+		setTimeout(function () {
+			headerNav.classList.add('menu-open');
+		}, 500);
+		hamburger.classList.add('active');
+		state = true;
+		// setTimeout(function () {
+		// 	headerNav.removeAttribute('style');
+		// }, 400);
+	}
+
+	function closeMenu() {
+		headerNav.style.transition = '0.4s ease 0s';
+
+		setTimeout(function () {
+			headerNav.classList.remove('menu-open');
+		}, 500);
+		hamburger.classList.remove('active');
+		state = false;
+		enableBodyScroll(headerNav);
+		// setTimeout(function () {
+		// 	headerNav.removeAttribute('style');
+		// }, 400);
+	}
+}
+
+mobileMenu();
